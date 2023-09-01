c01d7fea9f629a8c9bd953ad75b19314a24357a8
Amine Khaldi <amine.khaldi@reactos.org>
2023-09-01 22:50:00 +0100

    We don't need these anymore.

--- tests/wallet/nft_wallet/config.py
+++ tests/wallet/nft_wallet/config.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
 
-parallel = True
 job_timeout = 115
 checkout_blocks_and_plots = True
--- tests/wallet/rpc/config.py
+++ tests/wallet/rpc/config.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
 
-parallel = True
 checkout_blocks_and_plots = True
 job_timeout = 90
